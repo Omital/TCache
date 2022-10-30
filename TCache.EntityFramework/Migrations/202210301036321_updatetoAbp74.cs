822fb811e59d561627c88792f10f349d08683bfa
Omid Talebi <omid.talebi@hotmail.com>
2022-10-30 14:10:57 +0330

    update to abp 7.4

---
+++ TCache.EntityFramework/Migrations/202210301036321_updatetoAbp74.cs
@@ -0,0 +1,19 @@
+﻿namespace TCache.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class updatetoAbp74 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AbpNotifications", "TargetNotifiers", c => c.String(maxLength: 1024));
+            AlterColumn("dbo.AbpUserNotifications", "TargetNotifiers", c => c.String(maxLength: 1024));
+        }
+
+        public override void Down()
+        {
+            AlterColumn("dbo.AbpUserNotifications", "TargetNotifiers", c => c.String());
+            AlterColumn("dbo.AbpNotifications", "TargetNotifiers", c => c.String());
+        }
+    }
+}
